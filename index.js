78a8ce2ebe54c3cb39fef5dba6012c3af185b945
HabibaFarouk <habiba.mfaroukk@gmail.com>
2025-12-04 19:25:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,28 @@ import express from "express";
 import mongoose from "mongoose";
 import Campaign from "./Models/CampaignSchema.js";
 import Donation from "./Models/DonationSchema.js";
+import User from "./Models/UserSchema.js";
 
 const app = express();
 
-const addDonation = async (DonationData) => {
-    const donation = await Donation.create(DonationData);
+const addDonation = async (donationData) => {
+    const donation = await Donation.create(donationData);
     const { campaignId, amount } = donation;
     const updatedCampaign = await Campaign.findByIdAndUpdate(
       campaignId,
       { $inc: { amountCollected: amount } },
       { new: true }
-    );
+      );
 };
 
 
 app.use(express.json());
 
-app.get("/api/campaigns/",async(req,res)=>{
+app.get("/", (req, res) => {
+    res.send("CrowdTrust is running");
+});
+
+app.get("/campaigns",async(req,res)=>{
     const campaigns = await Campaign.find();
     res.json(
     campaigns.map(c => ({
@@ -30,37 +35,29 @@ app.get("/api/campaigns/",async(req,res)=>{
   );
 });
 
-app.post("/api/donations/",async(req,res)=>{
+app.post("/donations",async(req,res)=>{
     const newDonation = req.body;
-    await addDonation(newDonation);
+    await addDonation(newDonation).catch(error => res.status(500).json({ error: "Donation failed" }));
     res.json({ message: "Donation added" });
     
 });
 
 
-async function main() {
-    // Connect to MongoDB
+
+// Connect to MongoDB
   async function main() {
-  try {
-    await mongoose.connect("mongodb://habibamfaroukk_db_user:BoqJQLXgJzldvjB3@cluster0-shard-00-00.26s4lb0.mongodb.net:27017,cluster0-shard-00-01.26s4lb0.mongodb.net:27017,cluster0-shard-00-02.26s4lb0.mongodb.net:27017/CrowdTrust?ssl=true&replicaSet=atlas-xxxxx-shard-0&authSource=admin&retryWrites=true&w=majority");
+  await mongoose.connect("mongodb+srv://habibamfaroukk_db_user:BoqJQLXgJzldvjB3@cluster0.26s4lb0.mongodb.net/CrowdTrust?retryWrites=true&w=majority");
 
     console.log("Connected to MongoDB");
-
-    // You can start your server here, e.g.,
-    // app.listen(3000, () => console.log("Server running on port 3000"));
-  } catch (err) {
-    console.error("MongoDB connection error:", err);
-    process.exit(1); // Exit the process if connection fails
-  }
-}
-
-main();
-
-
-    // Once database connection is ready, start listening for requests
+    Campaign.find().then(campaigns => {
+        console.log("Campaign from MongoDB:", Campaign);
+    }).catch(err => {
+        console.error("Error fetching Campaign:", err);
+    });
+    
     app.listen(3000, () => {
         console.log("Server is running on http://localhost:3000");
     });
-}
+  } 
 
 main().catch(err => console.log(err));
